ad652084086a24e5ac700a48b92e01e970376208
Yulia51188 <sun51188@gmail.com>
2019-02-14 16:30:04 +0300

    Debug HH parser
    Add def join_vacancies_pages(data_pages),
    def get_salary_data(data_pages)

--- hh_parser.py
+++ hh_parser.py
@@ -13,6 +13,13 @@ def parse_arguments():
         nargs='+',
         help='necessary vacancy name, default is "программист"',
     )
+    parser.add_argument(
+        '-p', '--search_period',
+        default=30,
+        type=int,
+        help='the maximum allowed period of time from the moment of opening'
+                ' a vacancy (in days)',
+    )
     return parser.parse_args()
 
 
@@ -39,16 +46,45 @@ def get_vacancies(vacancy_name, hh_url="https://api.hh.ru/vacancies", area="1",
     response = requests.get(hh_url, params=params)
     response_data= response.json()
     page_number = response_data["pages"]    
-    #print("Total: {0}".format(page_number))
-    response_data_list = [get_vacancies_page(hh_url, params, index) for index in range(0,page_number)]
+    response_data_list = [get_vacancies_page(hh_url, params, index) 
+                            for index in range(0,page_number)]
     return response_data_list
+
+
+def join_vacancies_pages(data_pages):
+    vacancy_list = []
+    for data_page in data_pages:
+        for item in data_page["items"]:
+            vacancy_list.append(item)
+    return vacancy_list
+
+
+def get_salary_data(data_pages):
+    salary_data = {}
+    salary_data["total"] = data_pages[0]["found"]
+    vacancies = join_vacancies_pages(data_pages)
+    salary_data["items"] = []
+    for vacancy in vacancies:
+        salary_data["items"].append({
+            "id": vacancy["id"],
+            "name": vacancy["name"],
+            "salary": vacancy["salary"],
+        })
+    return salary_data
 
 
 def main():
     args = parse_arguments()
     vacancy_name = ' '.join(args.vacancy_name)
-    vacancies = get_vacancies(vacancy_name)
-    print("Get {0} pages".format(len(vacancies)))
+    search_period = str(args.search_period)
+    print("Searching for '{0}'...".format(vacancy_name))
+    print(search_period)    
+    pages_with_vacancies = get_vacancies(
+        vacancy_name, 
+        period=args.search_period
+    )
+    print("Get {0} pages".format(len(pages_with_vacancies)))
+    salary = get_salary_data (pages_with_vacancies)
 
 
 if __name__ == '__main__':
