5b4319d9a3a03bed60618c9a91013863ee55b4fd
Yulia51188 <sun51188@gmail.com>
2019-02-19 23:00:06 +0300

    Update salary_data structure

--- salary_stat.py
+++ salary_stat.py
@@ -28,20 +28,20 @@ def calc_mean_salary_for_speciality(speciality_salaries, koef_from = 1.2,
                                     koef_to = 0.8):
     mean_salaries = []
     for item in speciality_salaries:
-        if not item["salary"]["currency"] == "RUR":
+        if not item["salary_currency"] == "RUR":
             continue
-        if item["salary"]["from"] is None and item["salary"]["to"] is None:
+        if item["salary_from"] is None and item["salary_to"] is None:
             continue
-        if item["salary"]["from"] is None:
-            mean_salaries.append(float(item["salary"]["to"]) * koef_to)
+        if item["salaryfrom"] is None:
+            mean_salaries.append(float(item["salary_to"]) * koef_to)
             continue
-        if item["salary"]["to"] is None:
-            mean_salaries.append(float(item["salary"]["from"]) * koef_from) 
+        if item["salary_to"] is None:
+            mean_salaries.append(float(item["salary_from"]) * koef_from) 
             continue
         mean_salaries.append(
             mean([
-                float(item["salary"]["to"]), 
-                float(item["salary"]["from"])
+                float(item["salary_to"]), 
+                float(item["salary_from"])
             ])
         )              
     return (len(mean_salaries), mean(mean_salaries))
@@ -57,6 +57,13 @@ def create_salary_stat_object(lang, vacancies_with_salary):
     return salary_data
 
 
+def pretty_print(salaies_data):
+    pass
+
+
+
+
+
 def main():
     args = parse_arguments()
     salaries_data = []
@@ -64,18 +71,27 @@ def main():
     vacancy_template = "Программист"
     for index, lang in enumerate(args.lang_list):
         vacancy_name = ' '.join([vacancy_template, lang])
-        print("Searching for '{0}'...".format(vacancy_name))
-        vacancies_with_salary = hh_parser.get_salary_data(     
+        print("Searching for '{0}' in HeadHunter...".format(vacancy_name))
+        hh_vacancies_with_salary = hh_parser.get_salary_data(     
             vacancy_name,
             period=search_period,
         )
         print("Proccess mean salary for speciality '{0}'...".format(vacancy_name))
-        salaries_data.append(create_salary_stat_object(lang, 
-                                                    vacancies_with_salary))
-    for salary_data in salaries_data:
-        print()
-        for key in sorted(salary_data):
-            print('{key}: {value}'.format(key=key, value=salary_data[key]))
+        hh_salaries_data.append(create_salary_stat_object(lang, 
+                                                    hh_vacancies_with_salary))
+        #TO DO: debug superjob parsing
+        #print("Searching for '{0}' in SuperJob...".format(vacancy_name))
+        # sj_vacancies_with_salary = superjob_parser.get_salary_data(     
+        #     vacancy_name,
+        #     period=search_period,
+        # )
+        #sj_salaries_data.append(create_salary_stat_object(lang, 
+        #                                            sj_vacancies_with_salary))
+    pretty_print(hh_salaries_data)
+    # for salary_data in salaries_data:
+    #     print()
+    #     for key in sorted(salary_data):
+    #         print('{key}: {value}'.format(key=key, value=salary_data[key]))
         
 
 if __name__ == '__main__':
